fe4a5dc454f7f67ef91e3ff384c079667e386ed3
npappas27 <nickpappas@austins-mbp.lan>
2022-03-29 14:40:01 -0400

    init

---
+++ square-take-home/square-take-home/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  square-take-home
+//
+//  Created by Nick Pappas on 3/29/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
